ed17f7b8122259ffec38d4faa22a895f009424b1
Oleksii Fedorov <waterlink000@gmail.com>
2016-05-21 23:35:02 +0200

    Implement bowling game

--- public/javascripts/BowlingGame.js
+++ public/javascripts/BowlingGame.js
@@ -2,6 +2,11 @@ var BowlingGame = function() {
   this.rolls = []
 }
 
+BowlingGame.allPins = 10
+BowlingGame.numberOfFrames = 10
+BowlingGame.rollsInFrame = 2
+BowlingGame.rollsInStrikeFrame = 1
+
 BowlingGame.prototype.roll = function(pins) {
   this.rolls.push(pins)
 }
@@ -9,13 +14,46 @@ BowlingGame.prototype.roll = function(pins) {
 BowlingGame.prototype.score = function() {
   var totalScore = 0
 
-  for (var i = 0; i < this.rolls.length; i++) {
-    totalScore += this.rolls[i]
+  var currentRoll = 0
+  for (var frame = 0; frame < BowlingGame.numberOfFrames; frame++) {
+    var firstRoll = this.rolls[currentRoll]
+    var secondRoll = this.rolls[currentRoll + 1]
 
-    if (this.rolls[i - 1] + this.rolls[i] == 10) {
-      totalScore += this.rolls[i + 1]
+    if (this.isStrike(firstRoll)) {
+      totalScore += this.scoreForStrike(currentRoll)
+      currentRoll += BowlingGame.rollsInStrikeFrame
+    } else if (this.isSpare(firstRoll, secondRoll)) {
+      totalScore += this.scoreForSpare(currentRoll)
+      currentRoll += BowlingGame.rollsInFrame
+    } else {
+      totalScore += firstRoll + secondRoll
+      currentRoll += BowlingGame.rollsInFrame
     }
   }
 
   return totalScore
+}
+
+BowlingGame.prototype.isStrike = function(firstRoll) {
+  return firstRoll == BowlingGame.allPins
+}
+
+BowlingGame.prototype.scoreForStrike = function(currentRoll) {
+  return BowlingGame.allPins + this.bonusForStrike(currentRoll)
+}
+
+BowlingGame.prototype.bonusForStrike = function(currentRoll) {
+  return this.rolls[currentRoll + 1] + this.rolls[currentRoll + 2]
+}
+
+BowlingGame.prototype.isSpare = function(firstRoll, secondRoll) {
+  return firstRoll + secondRoll == BowlingGame.allPins
+}
+
+BowlingGame.prototype.scoreForSpare = function(currentRoll) {
+  return BowlingGame.allPins + this.bonusForSpare(currentRoll)
+}
+
+BowlingGame.prototype.bonusForSpare = function(currentRoll) {
+  return this.rolls[currentRoll + 2]
 }
--- spec/javascripts/BowlingGameSpec.js
+++ spec/javascripts/BowlingGameSpec.js
@@ -17,9 +17,33 @@ describe("BowlingGame", function() {
     })
   })
 
-  describe("game of ones with shifted spare", function() {
+  describe("game of ones with shifted 'spare'", function() {
     it("has a score of 10 + 18 = 28", function() {
       expect(shiftedSpareAndOnes().score()).toEqual(28)
+    })
+  })
+
+  describe("game of ones with spare at the end", function() {
+    it("has a score of 18 + 10 + 1 = 29", function() {
+      expect(onesAndSpare().score()).toEqual(29)
+    })
+  })
+
+  describe("game of ones with one strike", function() {
+    it("has a score of 10 + 1 + 1 + 18 = 30", function() {
+      expect(strikeAndOnes().score()).toEqual(30)
+    })
+  })
+
+  describe("game of ones with strike at the end", function() {
+    it("has a score of 18 + 10 + 1 + 1 = 30", function() {
+      expect(onesAndStrike().score()).toEqual(30)
+    })
+  })
+
+  describe("golden game", function() {
+    it("has a score of 300", function() {
+      expect(goldenGame().score()).toEqual(300)
     })
   })
 
@@ -48,6 +72,45 @@ describe("BowlingGame", function() {
     game.roll(7)
     for (var i = 0; i < 17; i++) {
       game.roll(1)
+    }
+    return game
+  }
+
+  function onesAndSpare() {
+    var game = new BowlingGame()
+    for (var i = 0; i < 18; i++) {
+      game.roll(1)
+    }
+    game.roll(3)
+    game.roll(7)
+    game.roll(1)
+    return game
+  }
+
+  function strikeAndOnes() {
+    var game = new BowlingGame()
+    game.roll(10)
+    for (var i = 0; i < 18; i++) {
+      game.roll(1)
+    }
+    return game
+  }
+
+  function onesAndStrike() {
+    var game = new BowlingGame()
+    for (var i = 0; i < 18; i++) {
+      game.roll(1)
+    }
+    game.roll(10)
+    game.roll(1)
+    game.roll(1)
+    return game
+  }
+
+  function goldenGame() {
+    var game = new BowlingGame()
+    for (var i = 0; i < 12; i++) {
+      game.roll(10)
     }
     return game
   }
